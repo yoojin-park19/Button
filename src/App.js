93347af7617ed6469beab1328d6a1140224c04c7
Sangdon1029 <gorist@likelion.org>
2022-03-21 01:57:53 +0900

    :memo:Test페이지 UI 구성

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomeIndexPage from './pages/home';
 import MainIndexPage from './pages/main';
 import ResultIndexPage from './pages/result';
+import TestIndexPage from './pages/test';
 function App() {
   return (
     <div className="App">
@@ -10,6 +11,7 @@ function App() {
           <Route path="/" exact component={HomeIndexPage} />
           <Route path="/main" exact component={MainIndexPage} />
           <Route path="/result" exact component={ResultIndexPage} />
+          <Route path="/test" exact component={TestIndexPage} />
         </Switch>
       </BrowserRouter>
     </div>
