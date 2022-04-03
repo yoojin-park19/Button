5fe50121e4d3a894ea68c88312f22c992e93a9bc
Sangdon1029 <gorist@likelion.org>
2022-04-04 01:23:23 +0900

    :lipstick: About_UI 수정

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import styled from '@emotion/styled';
-import { AboutMain } from '../components/About/AboutMain.jsx';
-
+import { AboutMain } from '../components/About/AboutMain';
+import { Global, css } from '@emotion/react';
 const AboutIndexPage = () => {
   return (
-    <About>
-      <AboutMain />
-    </About>
+    <>
+      <GlobalStyle> </GlobalStyle>
+      <About>
+        <AboutMain />
+      </About>
+    </>
   );
 };
-const About = styled.section``;
+const style = css`
+  body {
+    background-color: #febfa2;
+  }
+`;
+const About = styled.section`
+  background-color: #febfa2;
+`;
+
+const GlobalStyle = () => {
+  return <Global styles={style} />;
+};
 export default AboutIndexPage;
