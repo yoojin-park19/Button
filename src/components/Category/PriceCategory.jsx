0370fc25a66dcca6dcfd94162adb4d343e703e36
Sangdon1029 <gorist@likelion.org>
2022-04-21 16:54:54 +0900

    :iphone: [720px] 가격별 보기 미디어쿼리 적용

--- src/components/Category/PriceCategory.jsx
+++ src/components/Category/PriceCategory.jsx
@@ -6,29 +6,43 @@ import { Font, COLOR } from '../../constants';
 import { Cards } from './CategoryItem';
 
 export const PriceCategory = () => {
+  let [int1, setInt1] = useState(0);
+  let [int2, setInt2] = useState(0);
+  let [int3, setInt3] = useState(0);
+  let [int4, setInt4] = useState(0);
+  let [int5, setInt5] = useState(0);
   const [active1, setActive1] = useState(false);
   const [active2, setActive2] = useState(false);
   const [active3, setActive3] = useState(false);
   const [active4, setActive4] = useState(false);
   const [active5, setActive5] = useState(false);
-  const [price1, setPrice1] = useState([]);
-  const [price2, setPrice2] = useState([]);
-  const [price3, setPrice3] = useState([]);
-  const [price4, setPrice4] = useState([]);
-  const [price5, setPrice5] = useState([]);
+  let [price1, setPrice1] = useState([]);
+  let [price2, setPrice2] = useState([]);
+  let [price3, setPrice3] = useState([]);
+  let [price4, setPrice4] = useState([]);
+  let [price5, setPrice5] = useState([]);
   function pricecheck1() {
     setActive1(!active1);
+    setInt1(int1++);
+    if (int1 === 1) {
+      setPrice1((price1 = []));
+      setInt1((int1 = 0));
+    }
     if (active2) {
       setActive2(!active2);
+      setPrice2((price2 = []));
     }
     if (active3) {
       setActive3(!active3);
+      setPrice3((price3 = []));
     }
     if (active4) {
       setActive4(!active4);
+      setPrice4((price4 = []));
     }
     if (active5) {
       setActive5(!active5);
+      setPrice5((price5 = []));
     }
     Perfume.map((item, index) => {
       if (item.price < 100000) {
@@ -39,17 +53,26 @@ export const PriceCategory = () => {
   }
   function pricecheck2() {
     setActive2(!active2);
+    setInt2(int2++);
+    if (int2 === 1) {
+      setPrice2((price2 = []));
+      setInt2((int2 = 0));
+    }
     if (active1) {
       setActive1(!active1);
+      setPrice1((price1 = []));
     }
     if (active3) {
       setActive3(!active3);
+      setPrice3((price3 = []));
     }
     if (active4) {
       setActive4(!active4);
+      setPrice4((price4 = []));
     }
     if (active5) {
       setActive5(!active5);
+      setPrice5((price5 = []));
     }
     Perfume.map((item, index) => {
       if (item.price < 150000 && item.price > 100000) {
@@ -60,14 +83,23 @@ export const PriceCategory = () => {
   }
   function pricecheck3() {
     setActive3(!active3);
+    setInt3(int3++);
+    if (int3 === 1) {
+      setPrice3((price3 = []));
+      setInt3((int3 = 0));
+    }
     if (active1) {
       setActive1(!active1);
+      setPrice1((price1 = []));
     } else if (active2) {
       setActive2(!active2);
+      setPrice2((price2 = []));
     } else if (active4) {
       setActive4(!active4);
+      setPrice4((price4 = []));
     } else if (active5) {
       setActive5(!active5);
+      setPrice5((price5 = []));
     }
     Perfume.map((item, index) => {
       if (item.price < 200000 && item.price > 150000) {
@@ -79,14 +111,23 @@ export const PriceCategory = () => {
 
   function pricecheck4() {
     setActive4(!active4);
+    setInt4(int4++);
+    if (int4 === 1) {
+      setPrice4((price4 = []));
+      setInt4((int4 = 0));
+    }
     if (active1) {
       setActive1(!active1);
+      setPrice1((price1 = []));
     } else if (active2) {
       setActive2(!active2);
+      setPrice2((price2 = []));
     } else if (active3) {
       setActive3(!active3);
+      setPrice3((price3 = []));
     } else if (active5) {
       setActive5(!active5);
+      setPrice5((price5 = []));
     }
     Perfume.map((item, index) => {
       if (item.price < 300000 && item.price > 200000) {
@@ -97,14 +138,23 @@ export const PriceCategory = () => {
   }
   function pricecheck5() {
     setActive5(!active5);
+    setInt5(int5++);
+    if (int5 === 1) {
+      setPrice5((price5 = []));
+      setInt5((int5 = 0));
+    }
     if (active1) {
       setActive1(!active1);
+      setPrice1((price1 = []));
     } else if (active2) {
       setActive2(!active2);
+      setPrice2((price2 = []));
     } else if (active3) {
       setActive3(!active3);
+      setPrice3((price3 = []));
     } else if (active4) {
       setActive4(!active4);
+      setPrice4((price4 = []));
     }
     Perfume.map((item, index) => {
       if (300000 < item.price) {
@@ -267,24 +317,24 @@ const PricePage = styled.ul`
     top: 0px;
     z-index: 999;
     left: -20px;
-    width: 100%;
+    width: 100vw;
     .assortList {
       display: flex;
-      width: 80px;
+      width: 100vw;
       height: 80px;
       button {
-        width: 150px;
+        width: 100%;
         height: 80px;
         color: white;
         font-family: ${Font.gowun};
-        text-align: left;
+        text-align: center;
         font-size: 13px;
         border: none;
         background-color: ${COLOR.figma};
         color: #000;
         &:hover {
           background-color: ${COLOR.right};
-          width: 150px;
+          width: 100%;
           height: 80px;
         }
       }
