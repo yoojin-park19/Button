97dcbdad172e4e884a0449f0794d790b05b0fde2
Sangdon1029 <gorist@likelion.org>
2022-05-23 15:51:14 +0900

    :fire:Delete Scroll Event

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -5,51 +5,10 @@ import { FirstMain } from '../components/Main/FirstMain';
 import { SecondMain } from '../components/Main/SecondMain';
 import { ThirdMain } from '../components/Main/ThirdMain';
 import { ForthMain } from '../components/Main/ForthMain';
-import { useState, useRef, useEffect } from 'react';
 
 const MainIndexPage = () => {
-  let scrollRef = useRef();
-  let [scrollYY, setScrollYY] = useState(0);
-  let [allowed, setAllowed] = useState(false);
-  useEffect(() => {
-    const wheelHandler = (e) => {
-      const { deltaY } = e;
-      const pageHeight = window.innerHeight;
-      if (0 < deltaY && scrollYY === 0) {
-        window.scrollTo({
-          top: pageHeight + 100,
-          behavior: 'smooth',
-        });
-        setScrollYY((scrollYY = 1));
-      } else if (deltaY > 0 && scrollYY === 1) {
-        window.scrollTo({
-          top: pageHeight * 2 + 100,
-          behavior: 'smooth',
-        });
-        setScrollYY((scrollYY = 2));
-      } else if (deltaY < 0 && scrollYY === 2) {
-        window.scrollTo({
-          top: pageHeight + 100,
-          behavior: 'smooth',
-        });
-        setScrollYY((scrollYY = 1));
-      } else if (deltaY < 0 && scrollYY === 1) {
-        window.scrollTo({
-          top: 0,
-          behavior: 'smooth',
-        });
-        setScrollYY((scrollYY = 0));
-      }
-    };
-    const scrollRefCurrent = scrollRef.current;
-    scrollRefCurrent.addEventListener('wheel', wheelHandler);
-    return () => {
-      scrollRefCurrent.removeEventListener('wheel', wheelHandler);
-    };
-  }, [scrollRef, scrollYY]);
-
   return (
-    <Container ref={scrollRef}>
+    <Container>
       <MainHeader />
       <MainPage>
         <FirstMain />
